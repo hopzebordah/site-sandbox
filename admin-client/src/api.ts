869a968a678f796b56e242db903a3548b227ea4f
alex.peters <alex.peters@wavv.com>
2023-01-31 16:57:13 -0600

    generalized axios function

--- admin-client/src/api.ts
+++ admin-client/src/api.ts
@@ -1,12 +1,29 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 
 export const verifyLogin = async (): Promise<boolean> => {
     try {
-        const response = await axios.get('/api/admin/login/verify', {
-            withCredentials: true,
+        const response = await apiRequest({
+            url: '/api/admin/login/verify',
         })
         return response.status === 200
     } catch (err) {
         return false
+    }
+}
+
+const apiRequest = (config: AxiosRequestConfig) => {
+    if (!config.url) throw new Error('Cannot make request to empty url')
+
+    const modifiedConfig: AxiosRequestConfig = {
+        method: 'GET',
+        ...config,
+        withCredentials: true,
+    }
+
+    try {
+        return axios(modifiedConfig)
+    } catch (err) {
+        console.error('Error making request', err)
+        throw err
     }
 }
