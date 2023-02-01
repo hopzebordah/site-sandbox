4354aad056c1a44588485257b2cdbfe23f85158e
alex.peters <alex.peters@wavv.com>
2023-01-31 23:00:40 -0600

    basic site looking good

--- server/src/controllers/dynamicPageRenderer.ts
+++ server/src/controllers/dynamicPageRenderer.ts
@@ -2,8 +2,12 @@ import express from 'express'
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    res.render('home.pug')
+})
+
 router.use('*', (req, res) => {
-    console.log(req)
+    const url = req.baseUrl + req.path
     res.render('404.pug')
 })
 
